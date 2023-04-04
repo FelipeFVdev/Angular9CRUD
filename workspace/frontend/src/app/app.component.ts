90a3b81ee8d8851d1849d68c6fb2eafda2cb2459
FelipeFVdev <feliep8@gmail.com>
2023-04-03 22:09:58 -0300

    Aula 1 até Aula 18

---
+++ workspace/frontend/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+
+// <!-- <h2>Olá {{ nome }}, voce está bem?</h2> -->
+export class AppComponent {
+  // nome = 'Maria';
+  
+}
